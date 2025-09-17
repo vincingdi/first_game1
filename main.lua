5f6fbd509a599215d48d52d9b6fa5edb34b227e9
kurta <114595453+Kurtahh@users.noreply.github.com>
2025-09-17 10:48:54 +0300

    Sukuriau player
    Sukuriau player.lua, jame padariau movement(wasd) ir boundary checking, kad negaletum is lango iseit su player

--- main.lua
+++ main.lua
@@ -1,9 +1,14 @@
+require("player")
+
 function love.load()
     number = 0
+
+    Player:load()
 end
 
 
 function love.update(dt)
+<<<<<<< Updated upstream
     number = number + 2
 end
 
@@ -12,3 +17,15 @@ function love.draw()
 end 
 
 --test :D
+=======
+    number = number + 1
+
+    Player:update(dt)
+end
+
+function love.draw()
+    love.graphics.print(number +1)
+
+    Player:draw()
+end
+>>>>>>> Stashed changes
