acd4fb146d1ae006fbf9327eca56f4b24665ef7c
kurta <kartjalas@gmail.com>
2025-09-18 17:36:14 +0300

    enemy that goes back and forth
    created an enemy that goes left and right (right by default)

--- main.lua
+++ main.lua
@@ -1,10 +1,12 @@
 require("player")
+require("enemy")
 require("victory_screen")
 
 function love.load()
     number = 0
 
     Player:load()
+    Enemy:load()
 end
 
 
@@ -12,10 +14,12 @@ function love.update(dt)
     number = number + 1
 
     Player:update(dt)
+    Enemy:update(dt)
 end
 
 function love.draw()
     love.graphics.print(number +1)
 
     Player:draw()
+    Enemy:draw()
 end
