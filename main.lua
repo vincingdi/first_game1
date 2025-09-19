b8539f4134b27f6c56c20206af2376bb5bdaf01c
vincingdi <110591050+vincingdi@users.noreply.github.com>
2025-09-19 17:14:35 +0300

    Remove commented-out bullet handling code from main.lua

--- main.lua
+++ main.lua
@@ -1,23 +1,19 @@
 require("player")
 require("enemy")
 require("victory_screen")
-require("shoot_bullets")
 
 function love.load()
     Player:load()
     Enemy:load()
-    -- Player:load_bullet()
 end
 
 function love.update(dt)
     Player:update(dt)
     Enemy:update(dt)
-    -- Player:update_shooting(dt)
  
 end
 
 function love.draw()
     Player:draw()
     Enemy:draw()
-    -- Player:draw_bullet()
 end
